6d4debbb0846e9bee9c54c6d2c87c8d20e46b3d3
Zenahr Barzani <zenmatica@gmail.com>
2020-10-03 17:36:44 +0200

    add frame rate changer function

--- video-preparer/src/main.py
+++ video-preparer/src/main.py
@@ -1,8 +1,23 @@
 from components import myComponent
 import ffmpeg
 
+TEST_FILE="D:/Open-Source/Animation Pipeline/video-preparer/data/i.mp4"
 
 def flip_horizontally(input_file):  
     """Output path same as file_path"""
-    ffmpeg.input(input_file).hflip().output('HF' + input_file).run()
+    ffmpeg.input(input_file).hflip().output('HF' + "-" + 'output.mp4').run()
+# flip_horizontally(TEST_FILE)
 
+def change_frame_rate(input_file, frame_rate=20):
+    # EQUIVALENT ffmpeg INVOCATION: ffmpeg -y -i i.mp4 -vf "setpts=1.25*PTS" -r 8 o.mp4
+    (
+    ffmpeg
+    .input(input_file)
+    .zoompan(fps=frame_rate)
+    .setpts('1.25*PTS')
+    .output(filename="output.mp4")
+    .overwrite_output()
+    .run(capture_stdout=0)
+    )
+
+change_frame_rate(TEST_FILE)
