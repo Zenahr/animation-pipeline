a8dd14ed5d2ba655fd0e1b968ec241c948897d19
Zenahr Barzani <zenmatica@gmail.com>
2020-10-03 17:00:59 +0200

    add horizontal flip function

--- video-preparer/src/main.py
+++ video-preparer/src/main.py
@@ -1,3 +1,8 @@
 from components import myComponent
+import ffmpeg
 
-print("hello from main.py")
+
+def flip_horizontally(input_file):  
+    """Output path same as file_path"""
+    ffmpeg.input(input_file).hflip().output('HF' + input_file).run()
+
